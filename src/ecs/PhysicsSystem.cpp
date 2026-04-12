aa436690210bdc002d3423c7e00876ad2193add6
Kamil <lekamilthemage@gmail.com>
2026-04-12 11:58:07 +0200

    PhysicsSystem: extract struct for AABBs

--- src/ecs/PhysicsSystem.cpp
+++ src/ecs/PhysicsSystem.cpp
@@ -4,6 +4,20 @@ using namespace ecs::events;
 
 namespace ecs
 {
+    struct AABB
+    {
+        float left, right, top, bottom;
+    };
+    static AABB GetAABB(Vector2 pos, BoundingBoxComponent* box)
+    {
+        return {
+            pos.x + box->offset.x,
+            pos.x + box->offset.x + box->size.x,
+            pos.y + box->offset.y,
+            pos.y + box->offset.y + box->size.y,
+        };
+    }
+
     void PhysicsSystem::Update()
     {
         for (auto [entity, physics, boundingBox, transform] : world->GetEntities<PhysicsComponent, BoundingBoxComponent, TransformComponent>())
@@ -33,15 +47,8 @@ namespace ecs
                 }
 
                 // Get world-space AABBs
-                float aLeft = transform->position.x + boundingBox->offset.x;
-                float aRight = aLeft + boundingBox->size.x;
-                float aTop = transform->position.y + boundingBox->offset.y;
-                float aBottom = aTop + boundingBox->size.y;
-
-                float bLeft = otherTransform->position.x + otherBox->offset.x;
-                float bRight = bLeft + otherBox->size.x;
-                float bTop = otherTransform->position.y + otherBox->offset.y;
-                float bBottom = bTop + otherBox->size.y;
+                AABB a = GetAABB(transform->position, boundingBox);
+                AABB b = GetAABB(otherTransform->position, otherBox);
 
                 // Skip if there's no overlap
                 if (((a.left >= b.right) || (a.right <= b.left) || (a.top >= b.bottom) || (a.bottom <= b.top)))
@@ -57,29 +64,26 @@ namespace ecs
                 {
                     // Use the pre-move position to determine which side to push to
                     // This also prevents overshooting collisions.
-                    float prevBottom = prevPosition.y + boundingBox->offset.y + boundingBox->size.y;
-                    float prevTop = prevPosition.y + boundingBox->offset.y;
-                    float prevRight = prevPosition.x + boundingBox->offset.x + boundingBox->size.x;
-                    float prevLeft = prevPosition.x + boundingBox->offset.x;
+                    AABB prev = GetAABB(prevPosition, boundingBox);
 
-                    if (prevBottom <= bTop)
+                    if (prev.bottom <= b.top)
                     {
-                        transform->position.y = bTop - boundingBox->offset.y - boundingBox->size.y;
+                        transform->position.y = b.top - boundingBox->offset.y - boundingBox->size.y;
                         transform->velocity.y = 0.0f;
                         physics->isGrounded = true;
                     }
-                    else if (prevTop >= bBottom)
+                    else if (prev.top >= b.bottom)
                     {
-                        transform->position.y = bBottom - boundingBox->offset.y;
+                        transform->position.y = b.bottom - boundingBox->offset.y;
                         transform->velocity.y = 0.0f;
                     }
-                    else if (prevRight <= bLeft)
+                    else if (prev.right <= b.left)
                     {
-                        transform->position.x = bLeft - boundingBox->offset.x - boundingBox->size.x;
+                        transform->position.x = b.left - boundingBox->offset.x - boundingBox->size.x;
                     }
-                    else if (prevLeft >= bRight)
+                    else if (prev.left >= b.right)
                     {
-                        transform->position.x = bRight - boundingBox->offset.x;
+                        transform->position.x = b.right - boundingBox->offset.x;
                     }
                 }
             }
