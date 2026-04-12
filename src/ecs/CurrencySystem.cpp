c6baa694fcfb81832ff41e713ac96e64a174c2dc
Kamil <lekamilthemage@gmail.com>
2026-04-12 12:00:01 +0200

    CurrencySystem: rename collision handling vars to be more semantic

--- src/ecs/CurrencySystem.cpp
+++ src/ecs/CurrencySystem.cpp
@@ -31,24 +31,24 @@ namespace ecs
     {
         if (const auto* e = std::get_if<Collision>(&event))
         {
-            auto* entityA = e->entity;
-            auto* entityB = e->otherEntity;
-            auto viewA = world->GetEntityView<InventoryComponent>(entityA);
-            if (!viewA.has_value())
+            auto* inventoryEntity = e->entity;
+            auto* currencyEntity = e->otherEntity;
+            auto inventoryView = world->GetEntityView<InventoryComponent>(inventoryEntity);
+            if (!inventoryView.has_value())
             {
-                // Swap entities so that A is the one with the inventory
+                // Swap entities so that the inventory holder is first
                 // Having a collision between 2 currency entities makes one take priority, yep - TODO?
-                std::swap(entityA, entityB);
-                viewA = world->GetEntityView<InventoryComponent>(entityA);
+                std::swap(inventoryEntity, currencyEntity);
+                inventoryView = world->GetEntityView<InventoryComponent>(inventoryEntity);
             }
 
-            if (viewA.has_value())
+            if (inventoryView.has_value())
             {
-                auto [inventory] = viewA.value();
-                auto* currency = entityB->GetComponent<CurrencyComponent>();
+                auto [inventory] = inventoryView.value();
+                auto* currency = currencyEntity->GetComponent<CurrencyComponent>();
                 if (currency)
                 {
-                    PickUpCurrency(inventory, entityB, currency);
+                    PickUpCurrency(inventory, currencyEntity, currency);
                 }
             }
         }
